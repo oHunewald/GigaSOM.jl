fe20ae65144468220fbf0089ebf9840b1eae84e0
ohunewald <oliver.hunewald@lih.lu>
2019-07-29 01:56:35 -0400

    modified for hpc testing

--- test/parallel.jl
+++ test/parallel.jl
@@ -12,15 +12,15 @@ end
 @everywhere using GigaSOM
 @everywhere using Distances
 
-
 # only use lineageMarkers for clustering
-(lineageMarkers,)= getMarkers(panel)
+
 cc = map(Symbol, lineageMarkers)
 dfSom = daf.fcstable[:,cc]
 
 som2 = initGigaSOM(dfSom, 10, 10)
 
-som2 = trainGigaSOM(som2, dfSom, epochs = 2, r = 6.0)
+som2 = trainGigaSOM(som2, dfSom, epochs = 1)
+@time som2 = trainGigaSOM(som2, dfSom, epochs = 10)
 
 winners = mapToGigaSOM(som2, dfSom)
 
