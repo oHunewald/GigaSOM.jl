a8cb2494231c310cc897e091b3f0d5882098d9a1
ohunewald <oliver.hunewald@lih.lu>
2019-07-29 01:45:37 -0400

    removed testing

--- test/parallel.jl
+++ test/parallel.jl
@@ -20,47 +20,9 @@ dfSom = daf.fcstable[:,cc]
 
 som2 = initGigaSOM(dfSom, 10, 10)
 
-@testset "Dimensions - parallel" begin
-    @test size(som2.codes) == (100,10)
-    @test som2.xdim == 10
-    @test som2.ydim == 10
-    @test som2.numCodes == 100
-end
-
 som2 = trainGigaSOM(som2, dfSom, epochs = 2, r = 6.0)
 
 winners = mapToGigaSOM(som2, dfSom)
-
-#test parallel
-@testset "Parallel" begin
-    codes = som2.codes
-    @test size(codes) == (100,10)
-
-    dfCodes = DataFrame(codes)
-    names!(dfCodes, Symbol.(som2.colNames))
-    CSV.write(genDataPath*"/parallelDfCodes.csv", dfCodes)
-    CSV.write(genDataPath*"/parallelWinners.csv", winners)
-
-    # load the ref data
-    refParallelDfCodes = CSV.File(refDataPath*"/refParallelDfCodes.csv") |> DataFrame
-    refParallelWinners = CSV.File(refDataPath*"/refParallelWinners.csv") |> DataFrame
-
-    # load the generated data
-    parallelDfCodes = CSV.File(genDataPath*"/parallelDfCodes.csv") |> DataFrame
-    parallelDfCodesTest = first(parallelDfCodes, 10)
-    parallelWinners = CSV.File(genDataPath*"/parallelWinners.csv") |> DataFrame
-    parallelWinnersTest = first(parallelWinners, 10)
-
-    # test the generated data against the reference data
-    @test refParallelWinners == parallelWinnersTest
-    @test refParallelDfCodes == parallelDfCodesTest
-
-    #test parallel
-    for (i, j) in zip(parallelDfCodes[:,1], refParallelDfCodes[:,1])
-        @test isapprox(i, j; atol = 0.001)
-    end
-
-end
 
 rmprocs(workers())
 
