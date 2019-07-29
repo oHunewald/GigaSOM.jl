720da48e5fe2b760e2b409c0c5a8b94c9f010568
ohunewald <oliver.hunewald@lih.lu>
2019-07-29 01:22:27 -0400

    added function myrange, for SharedArrays

--- src/satellites.jl
+++ src/satellites.jl
@@ -27,6 +27,19 @@ function gridRectangular(xdim, ydim)
     return grid
 end
 
+"""
+    myrange(q::SharedArray)
+"""
+function myrange(q::SharedArray)
+           idx = indexpids(q)
+           if idx == 0 # This worker is not assigned a piece
+               return 1:0, 1:0
+           end
+           nchunks = length(procs(q))
+           splits = [round(Int, s) for s in range(0, stop=size(q,1), length=nchunks+1)]
+           1:size(q,1), splits[idx]+1:splits[idx+1]
+end
+
 
 """
     gaussianKernel(x::Array{Float64, 1}, r::Float64)::Array{Float64, 1}
