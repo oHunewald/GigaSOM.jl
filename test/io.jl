9e64350af21b794b1ec4fd27f7dccd7b1499357f
oHunewald <o.hunewald@gmail.com>
2019-08-14 07:38:00 +0200

    removed testing

--- test/io.jl
+++ test/io.jl
@@ -1,12 +1,6 @@
 # Load and transform
 # build the general workflow to have the data ready
 
-#=
-using FCSFiles for loading
-as this function is only the basic parsing of the binary
-FCS, we need to see what functionality is missing and
-extend this in the original package
-=#
 # ENV["PYTHON"] = Sys.which("PYTHON")
 ENV["PYTHON"] = "/usr/local/bin/python3.6"
 
@@ -15,12 +9,10 @@ using GigaSOM, DataFrames, XLSX, CSV, Test, Random, Distributed, SHA, JSON, PyCa
 fcsparser = pyimport("fcsparser")
 
 dataPath = ("../data_felD1")
+# dataPath = ("../PBMC8_fcs_files")
 cd(dataPath)
 md = DataFrame(XLSX.readtable("metadata.xlsx", "Sheet1", infer_eltypes=true)...)
 panel = DataFrame(XLSX.readtable("panel.xlsx", "Sheet1", infer_eltypes=true)...)
-
-# two values are empty, do not try to parse them
-# markers = [convert(String, i) for i in df.values if i != nothing]
 
 lineageMarkers = vec(panel.Antigen[panel.Lineage .== 1, : ])
 cleanNames!(lineageMarkers)
@@ -35,39 +27,4 @@ fcsRaw = readFlowset(md, fcsparser)
 
 # create daFrame file
 daf = createDaFrame(fcsRaw, md, panel, lineageMarkers, functionalMarkers)
-
-# change the directory back to the current directory
-# cd(cwd)
-
-# CSV.write(dataPath*"/daf.csv", daf.fcstable)
-
-# @testset "Cleaning names" begin
-#     for i in eachindex(lineageMarkers)
-#         @test !in("-",i)
-#     end
-#     for i in eachindex(functionalMarkers)
-#         @test !in("-",i)
-#     end
-#     for (k,v) in fcsRaw
-#         colnames = names(v)
-#         for i in eachindex(colnames)
-#             @test !in("-",i)
-#         end
-#     end
-# end
-#
-# @testset "Checksums" begin
-#     cd(dataPath)
-#     filesNames = readdir()
-#     csDict = Dict()
-#     for f in filesNames
-#         cs = bytes2hex(sha256(f))
-#         csDict[f] = cs
-#     end
-#     cd(cwd*"/checkSums")
-#     csTest = JSON.parsefile("csTest.json")
-#     @test csDict == csTest
-#     cd(cwd)
-# end
-
-# cd(cwd)
+CSV.write("daf.csv", daf.fcstable)
