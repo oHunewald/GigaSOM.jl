2bb6c1af459c3507b7b1025697e546d4df060553
oHunewald <o.hunewald@gmail.com>
2019-09-27 09:14:48 +0200

    added myRange helper for shared arrays

--- src/satellites.jl
+++ src/satellites.jl
@@ -29,6 +29,20 @@ end
 
 
 """
+    myrange(q::SharedArray)
+"""
+function myrange(q::SharedArray)
+           idx = indexpids(q)
+           if idx == 0 # This worker is not assigned a piece
+               return 1:0, 1:0
+           end
+           nchunks = length(procs(q))
+           splits = [round(Int, s) for s in range(0, stop=size(q,1), length=nchunks+1)]
+           1:size(q,1), splits[idx]+1:splits[idx+1]
+end
+
+
+"""
     gaussianKernel(x, r::Float64)
 
 Return Gaussian(x) for μ=0.0 and σ = r/3.
