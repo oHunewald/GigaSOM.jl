742c99e1303a1185a17f591a986eceb6d07f2891
oHunewald <o.hunewald@gmail.com>
2019-10-02 09:15:44 +0200

    first send and receive example

---
+++ test/mpi_send_receive.jl
@@ -0,0 +1,28 @@
+import MPI
+using Statistics
+function main()
+
+    MPI.Init()
+    comm = MPI.COMM_WORLD
+    size = MPI.Comm_size(comm)
+    rank = MPI.Comm_rank(comm)
+    recv_mesg = zeros(100,1)
+
+    if rank == 0
+        send_mesg = rand(100, 1)
+        for i in 1:size-1
+            MPI.Send(send_mesg, i, 0, comm)
+        end
+    else
+        MPI.Recv!(recv_mesg, 0, 0, comm)
+    end
+
+    m = mean(recv_mesg, dims=1)
+    sleep(rank*2)
+    println("Results on rank ", rank, ": ", m)
+    MPI.Barrier(comm)
+    MPI.Finalize()
+
+end
+
+main()
