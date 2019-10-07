5990681f5d3e6b68afbd1d9373f2e11d5bca7c51
oHunewald <o.hunewald@gmail.com>
2019-10-07 13:38:48 +0200

    changed some test parameters

--- workflow/test_hpc.jl
+++ workflow/test_hpc.jl
@@ -1,9 +1,9 @@
 using GigaSOM, DataFrames, Random, Distributed
 
-p = addprocs(10, topology=:master_worker)
+p = addprocs(2, topology=:master_worker)
 @everywhere using GigaSOM
 
-dfSom = rand(50_000_000, 35)
+dfSom = rand(5_000_000, 35)
 
 som2 = initGigaSOM(dfSom, 10, 10)
 
@@ -14,29 +14,29 @@ println("Timing trainGigaSOM: ")
 rmprocs(workers())
 
 
-p = addprocs(20, topology=:master_worker)
-@everywhere using GigaSOM
-som2 = initGigaSOM(dfSom, 10, 10)
-# precompile
-trainGigaSOM(som2, dfSom, epochs = 1)
-println("Timing trainGigaSOM: ")
-@time som2 = trainGigaSOM(som2, dfSom, epochs = 10)
-rmprocs(workers())
-
-p = addprocs(40, topology=:master_worker)
-@everywhere using GigaSOM
-som2 = initGigaSOM(dfSom, 10, 10)
-# precompile
-trainGigaSOM(som2, dfSom, epochs = 1)
-println("Timing trainGigaSOM: ")
-@time som2 = trainGigaSOM(som2, dfSom, epochs = 10)
-rmprocs(workers())
-
-p = addprocs(80, topology=:master_worker)
-@everywhere using GigaSOM
-som2 = initGigaSOM(dfSom, 10, 10)
-# precompile
-trainGigaSOM(som2, dfSom, epochs = 1)
-println("Timing trainGigaSOM: ")
-@time som2 = trainGigaSOM(som2, dfSom, epochs = 10)
-rmprocs(workers())
+# p = addprocs(20, topology=:master_worker)
+# @everywhere using GigaSOM
+# som2 = initGigaSOM(dfSom, 10, 10)
+# # precompile
+# trainGigaSOM(som2, dfSom, epochs = 1)
+# println("Timing trainGigaSOM: ")
+# @time som2 = trainGigaSOM(som2, dfSom, epochs = 10)
+# rmprocs(workers())
+#
+# p = addprocs(40, topology=:master_worker)
+# @everywhere using GigaSOM
+# som2 = initGigaSOM(dfSom, 10, 10)
+# # precompile
+# trainGigaSOM(som2, dfSom, epochs = 1)
+# println("Timing trainGigaSOM: ")
+# @time som2 = trainGigaSOM(som2, dfSom, epochs = 10)
+# rmprocs(workers())
+#
+# p = addprocs(80, topology=:master_worker)
+# @everywhere using GigaSOM
+# som2 = initGigaSOM(dfSom, 10, 10)
+# # precompile
+# trainGigaSOM(som2, dfSom, epochs = 1)
+# println("Timing trainGigaSOM: ")
+# @time som2 = trainGigaSOM(som2, dfSom, epochs = 10)
+# rmprocs(workers())
