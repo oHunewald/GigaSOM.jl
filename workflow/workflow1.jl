fc2b2c533ac786e19a69a438443cbd30ac5091c8
ohunewald <oliver.hunewald@lih.lu>
2019-10-16 14:32:16 +0200

    path for metadata test loading

--- workflow/workflow1.jl
+++ workflow/workflow1.jl
@@ -5,12 +5,13 @@ checkDir()
 cwd = pwd()
 
 # datapath = "/Users/ohunewald/work/data_felD1"
+# datapath = "/Users/ohunewald/work/SysTact/test_metadata"
 # datapath = "/Users/ohunewald/work/SysTact/pre_data/SYSTACT_555_CD3pos"
-# datapath = "/home/users/ohunewald/systact/pre_data/SYSTACT_555_CD3pos"
-datapath = "/home/users/ohunewald/data"
+datapath = "/home/users/ohunewald/systact/test_metadata"
+# datapath = "/home/users/ohunewald/data"
 cd(datapath)
-md = DataFrame(XLSX.readtable("metadata_100.xlsx", "Sheet1", infer_eltypes=true)...)
-# md = DataFrame(XLSX.readtable("metadata.xlsx", "Sheet1", infer_eltypes=true)...)
+# md = DataFrame(XLSX.readtable("metadata_100.xlsx", "Sheet1", infer_eltypes=true)...)
+md = DataFrame(XLSX.readtable("metadata_test.xlsx", "Sheet1", infer_eltypes=true)...)
 # md = DataFrame(XLSX.readtable("metadata_small.xlsx", "Sheet1", infer_eltypes=true)...)
 panel = DataFrame(XLSX.readtable("panel.xlsx", "Sheet1", infer_eltypes=true)...)
 
@@ -25,40 +26,52 @@ daf = createDaFrame(fcsRaw, md, panel)
 cd(cwd)
 # using StatsPlots
 using Statistics
+try
+	using StatsBase
+catch
+	Pkg.add("StatsBase")
+end
 using StatsBase
+
+try
+	using MultivariateStats
+catch
+	Pkg.add("MultivariateStats")
+end
+
 using MultivariateStats
 
 #################################################################
 # Export the data for PCA
 #################################################################
-# dfall_median = aggregate(daf.fcstable, :sample_id, Statistics.median)
-#
-# # get the timepoints for each sample_id
-# # md.timepoint
-# md.condition
-#
-# T = convert(Matrix, dfall_median)
-# samples_ids = T[:,1]
-# T_reshaped = permutedims(convert(Matrix{Float64}, T[:, 2:end]), [2, 1])
-#
-# my_pca = StatsBase.fit(MultivariateStats.PCA, T_reshaped)
-#
-# yte = MultivariateStats.transform(my_pca,T_reshaped)
-#
-# df_pca = DataFrame(yte')
-# df_pca[:sample_id] = samples_ids
-#
-# # get the condition per sample id and add in DF
-# v1= df_pca.sample_id; v2=md.sample_id
-# idxs = indexin(v1, v2)
-# df_pca[:condition] = md.condition[idxs]
-# # df_pca[:timepoint] = md.timepoint[idxs]
-# CSV.write("pca_df.csv", df_pca)
+dfall_median = aggregate(daf.fcstable, :sample_id, Statistics.median)
+
+# get the timepoints for each sample_id
+# md.timepoint
+md.condition
+
+T = convert(Matrix, dfall_median)
+samples_ids = T[:,1]
+T_reshaped = permutedims(convert(Matrix{Float64}, T[:, 2:end]), [2, 1])
+
+my_pca = StatsBase.fit(MultivariateStats.PCA, T_reshaped)
+
+yte = MultivariateStats.transform(my_pca,T_reshaped)
+
+df_pca = DataFrame(yte')
+df_pca[:sample_id] = samples_ids
+
+# get the condition per sample id and add in DF
+v1= df_pca.sample_id; v2=md.sample_id
+idxs = indexin(v1, v2)
+df_pca[:condition] = md.condition[idxs]
+# df_pca[:timepoint] = md.timepoint[idxs]
+CSV.write("pca_df.csv", df_pca)
 
 #fix the seed
 Random.seed!(1)
 
-p = addprocs(4)
+p = addprocs(2)
 
 # @everywhere using DistributedArrays
 @everywhere using GigaSOM
@@ -89,6 +102,12 @@ codes = som2.codes
 # include("../src/io/cCluster.jl")
 # retrive the cluster ids
 # mc =  cc_plus(codes)
+try
+	using RCall
+catch
+	using Pkg
+	Pkg.add("RCall")
+end
 using RCall
 @rlibrary consens2
 
@@ -106,6 +125,7 @@ dfSom.cell_clustering = cell_clustering
 
 using Statistics
 using LinearAlgebra
+# Pkg.add("StatsBase")
 using StatsBase
 # median expression values per som node
 expr_median = aggregate(dfSom, :cell_clustering, median)
