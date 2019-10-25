2a33dbe2c2dcc1fecf86aa68c40062bbb920dbb8
ohunewald <oliver.hunewald@lih.lu>
2019-10-25 10:46:29 +0200

    two cclustering sizes, added sample_id to embed

--- workflow/workflow1.jl
+++ workflow/workflow1.jl
@@ -5,20 +5,19 @@ checkDir()
 cwd = pwd()
 
 # datapath = "/Users/ohunewald/work/data_felD1"
-# datapath = "/Users/ohunewald/work/SysTact/test_metadata"
+datapath = "/Users/ohunewald/work/SysTact/test_metadata"
 # datapath = "/Users/ohunewald/work/SysTact/pre_data/SYSTACT_555_CD3pos"
 # datapath = "/home/users/ohunewald/systact/test_metadata"
-datapath = "/home/users/ohunewald/systact/data"
+# datapath = "/home/users/ohunewald/systact/data"
 # datapath = "/home/users/ohunewald/data"
 cd(datapath)
 # md = DataFrame(XLSX.readtable("metadata_100.xlsx", "Sheet1", infer_eltypes=true)...)
-md = DataFrame(XLSX.readtable("metadata.xlsx", "Sheet1", infer_eltypes=true)...)
-# md = DataFrame(XLSX.readtable("metadata_test.xlsx", "Sheet1", infer_eltypes=true)...)
+# md = DataFrame(XLSX.readtable("metadata.xlsx", "Sheet1", infer_eltypes=true)...)
+md = DataFrame(XLSX.readtable("metadata_test.xlsx", "Sheet1", infer_eltypes=true)...)
 # md = DataFrame(XLSX.readtable("metadata_small.xlsx", "Sheet1", infer_eltypes=true)...)
 panel = DataFrame(XLSX.readtable("panel.xlsx", "Sheet1", infer_eltypes=true)...)
 
 lineageMarkers, functionalMarkers = getMarkers(panel)
-
 fcsRaw = readFlowset(md.file_name)
 cleanNames!(fcsRaw)
 
@@ -67,24 +66,21 @@ df_pca[:sample_id] = samples_ids
 v1= df_pca.sample_id; v2=md.sample_id
 idxs = indexin(v1, v2)
 df_pca[:condition] = md.condition[idxs]
-df_pca[:timepoint] = md.timepoint[idxs]
+# df_pca[:timepoint] = md.timepoint[idxs]
 CSV.write("pca_df.csv", df_pca)
 
 #fix the seed
 Random.seed!(1)
 
-p = addprocs(80)
+p = addprocs(2)
 
-# @everywhere using DistributedArrays
 @everywhere using GigaSOM
-# @everywhere using Distances
-# using Distances
 
 # only use lineageMarkers for clustering
 (lineageMarkers,)= getMarkers(panel)
 cc = map(Symbol, lineageMarkers)
 dfSom = daf.fcstable[:,cc]
-
+sample_id = daf.fcstable[:, :sample_id]
 som2 = initGigaSOM(dfSom, 10, 10)
 
 # som2 = trainGigaSOM(som2, dfSom, epochs = 1)
@@ -94,8 +90,9 @@ winners = mapToGigaSOM(som2, dfSom)
 
 CSV.write("winners.csv", winners)
 @time embed = embedGigaSOM(som2, dfSom, k=10, smooth=0.0, adjust=0.5)
-
-CSV.write("embed.csv", DataFrame(embed))
+embedDf = DataFrame(embed)
+insertcols!(embedDf, 1, sample_id = sample_id)
+CSV.write("embed.csv", embedDf)
 
 rmprocs(workers())
 
@@ -111,50 +108,52 @@ catch
 	Pkg.add("RCall")
 end
 using RCall
-@rlibrary consens2
-
-plot_outdir = "consensus_plots"
-nmc = 50
-codesT = Matrix(codes')
-mc = ConsensusClusterPlus_2(codesT, maxK = nmc, reps = 100,
-                           pItem = 0.9, pFeature = 1,
-                           clusterAlg = "hc", innerLinkage = "average", finalLinkage = "average",
-                           distance = "euclidean", seed = 1234)
-
-cell_clustering = mc[winners.index]
-
-dfSom.cell_clustering = cell_clustering
-
 using Statistics
 using LinearAlgebra
 # Pkg.add("StatsBase")
 using StatsBase
-# median expression values per som node
-expr_median = aggregate(dfSom, :cell_clustering, median)
-# expr_median_norm = deepcopy(expr_median)
+@rlibrary consens2
 
-# apply uniform scaling to all features (columns)
-# get the cell clustering column
-cc_aggregated = expr_median.cell_clustering
-# remove the cell clustering column
-tmp = expr_median[:, filter(x -> x != :cell_clustering, names(expr_median))]
-# get the column names
-c_names = names(tmp)
-tmp_matrix = Matrix(tmp)
-dt = fit(UnitRangeTransform, tmp_matrix')
-expr_med_norm = StatsBase.transform(dt, tmp_matrix')
-expr_med_norm = DataFrame(expr_med_norm')
-names!(expr_med_norm, c_names)
-# put back the column cell clustering
-expr_med_norm[:cell_clustering] = cc_aggregated
+plot_outdir = "consensus_plots"
+cclusters = [50, 25]
+# nmc = 50
+codesT = Matrix(codes')
+for nmc in cclusters
+	mc = ConsensusClusterPlus_2(codesT, maxK = nmc, reps = 100,
+	                           pItem = 0.9, pFeature = 1,
+	                           clusterAlg = "hc", innerLinkage = "average", finalLinkage = "average",
+	                           distance = "euclidean", seed = 1234)
 
-CSV.write("expr_median.csv", expr_median)
-CSV.write("expr_median_norm.csv", expr_med_norm)
-# sampleId = daf.fcstable[ : , :sample_id]
+	cell_clustering = mc[winners.index]
 
-cc_tbl = DataFrame(id = cell_clustering)
-CSV.write("cell_clustering.csv", cc_tbl)
+	dfSom.cell_clustering = cell_clustering
 
+	# median expression values per som node
+	expr_median = aggregate(dfSom, :cell_clustering, median)
+	# expr_median_norm = deepcopy(expr_median)
+
+	# apply uniform scaling to all features (columns)
+	# get the cell clustering column
+	cc_aggregated = expr_median.cell_clustering
+	# remove the cell clustering column
+	tmp = expr_median[:, filter(x -> x != :cell_clustering, names(expr_median))]
+	# get the column names
+	c_names = names(tmp)
+	tmp_matrix = Matrix(tmp)
+	dt = fit(UnitRangeTransform, tmp_matrix')
+	expr_med_norm = StatsBase.transform(dt, tmp_matrix')
+	expr_med_norm = DataFrame(expr_med_norm')
+	names!(expr_med_norm, c_names)
+	# put back the column cell clustering
+	expr_med_norm[:cell_clustering] = cc_aggregated
+
+	CSV.write("expr_median_consensus$nmc.csv", expr_median)
+	CSV.write("expr_median_norm_consensus$nmc.csv", expr_med_norm)
+	# sampleId = daf.fcstable[ : , :sample_id]
+
+	cc_tbl = DataFrame(id = cell_clustering)
+	CSV.write("cell_clustering_consensus$nmc.csv", cc_tbl)
+end
 
 cd(cwd)
 # # plotting embed in julia
