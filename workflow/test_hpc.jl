f936ee07ad296547bee18e5719b23c888e8b01b2
ohunewald <oliver.hunewald@lih.lu>
2019-10-25 07:39:46 +0200

    changed test

--- workflow/test_hpc.jl
+++ workflow/test_hpc.jl
@@ -2,27 +2,52 @@ using GigaSOM, DataFrames, Random, Distributed
 
 Random.seed!(1)
 
-# ! set the number of workers here:
-p = addprocs(2)
+try
+    using Distributions
+catch
+    Pkg.add("Distributions")
+end
+using Distributions
 
-@everywhere using GigaSOM
+function printHeader(m, n, g)
+    println("-------------------------------------------------")
+    println("Data size: $m")
+    println("-------------------------------------------------")
+    println("Grid Size: $g")
+    println("-------------------------------------------------")
+    println("Nunber of workers: $n")
+    println("-------------------------------------------------")
+    println("Timing trainGigaSOM: ")
+end
 
-# ! set the size of the Matrix here:
-# only change the the first dimension
-dfSom = rand(5_000_000, 40)
+gridsizes = [10, 20]
+myworkers = [1,4,20,40,80]
+dfSom = rand(50_000_000, 30)
 
-som2 = initGigaSOM(dfSom, 10, 10)
+for j in myworkers
+    if j > 1
+        p = addprocs(j)
+    end
+    @everywhere using GigaSOM
 
-# precompile
-trainGigaSOM(som2, dfSom, epochs = 1)
+    som2 = initGigaSOM(dfSom, 10, 10)
 
-println("Timing trainGigaSOM: ")
-@time som2 = trainGigaSOM(som2, dfSom, epochs = 10)
+    for g in gridsizes
+        som2 = initGigaSOM(dfSom, g, g)
 
-# precompile
-# embedGigaSOM(som2, dfSom, k=10, smooth=0.0, adjust=0.5)
-#
-# println("Timing embedGigaSOM: ")
-# @time embed = embedGigaSOM(som2, dfSom, k=10, smooth=0.0, adjust=0.5)
+        printHeader(size(dfSom), nworkers(), g)
+        som2 = trainGigaSOM(som2, DataFrame(dfSom), epochs = 1)
+        @time som2 = trainGigaSOM(som2, DataFrame(dfSom), epochs = 10)
 
-rmprocs(workers())
+        println("Timing embedSOM: ")
+        embed = embedGigaSOM(som2, DataFrame(dfSom), k=10, smooth=0.0, adjust=0.5)
+        @time embed = embedGigaSOM(som2, DataFrame(dfSom), k=10, smooth=0.0, adjust=0.5)
+
+
+    end
+
+    if j > 1
+        rmprocs(workers())
+    end
+
+end
